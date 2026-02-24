f64b9a38f5efbccd754b7f2d4a48c503b5f37983
Sandeep Kumar <84sandeepsiwani@gmail.com>
2026-02-24 23:43:17 +0530

    change after clone

---
+++ webint/src/Header.jsx
@@ -0,0 +1,10 @@
+
+export const Header = () => {
+    return (
+        <header className="header">
+            <img src="" alt="" />
+            <div className="div"></div>
+
+        </header>
+    );
+}
