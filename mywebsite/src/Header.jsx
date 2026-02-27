7a9459650c4a316fc87d436c66df4f46fa32f2e8
Sandeep Kumar <84sandeepsiwani@gmail.com>
2026-02-27 23:31:46 +0530

    form updat

--- mywebsite/src/Header.jsx
+++ mywebsite/src/Header.jsx
@@ -3,6 +3,12 @@ export const Header = () => {
     <div className="header">
       <h1>Websint</h1>
       <h3>comming soon .... </h3>
+      <form action="">
+        <input type="email" placeholder="Enter your email" />
+        <input type="email" placeholder="Enter your email" />
+        <input type="email" placeholder="Enter your email" />
+        <button type="submit">Notify Me</button>
+      </form>
     </div>
   );
 };
